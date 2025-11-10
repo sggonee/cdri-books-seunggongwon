8a400a2016ed8f56a0a65c222d0fd6141dd66384
sggonee <sggonee@gmail.com>
2025-11-10 17:52:26 +0900

    update layout of not found

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -3,12 +3,13 @@ import SearchNotFound from '@/components/template/search/NotFound';
 import Summary from '@/components/template/search/Summary';
 
 const Search = () => {
+  const search = [];
   return (
     <section>
       <h2>도서 검색</h2>
       <Form />
       <Summary count={0} />
-      <SearchNotFound />;
+      {search.length ? [] : <SearchNotFound />}
     </section>
   );
 };
