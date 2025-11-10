04f10c318b050c496127bf452ee9b2ef15119132
sggonee <sggonee@gmail.com>
2025-11-10 17:29:50 +0900

    add button component

---
+++ src/components/element/button/index.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx';
+import { ButtonHTMLAttributes } from 'react';
+import style from './Button.module.css';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'outline' | 'ghost';
+  size?: 'sm' | 'md' | 'lg';
+}
+
+const Button = ({ variant = 'primary', size = 'md', className, children, ...rest }: Props) => {
+  return (
+    <button className={clsx(style.button, style[variant], style[size], className)} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
