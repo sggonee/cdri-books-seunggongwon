d3a591dd060f43eb96617a28188ab180caac2865
sggonee <sggonee@gmail.com>
2025-11-10 17:58:37 +0900

    update layout of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import clsx from 'clsx';
 import { Outlet } from 'react-router-dom';
+import styles from './App.module.css';
 import Header from './components/layout/Header';
 
 function App() {
   return (
     <>
       <Header />
-      <main className="l-container">
+      <main className={clsx(styles.container, 'l-container')}>
         <Outlet />
       </main>
     </>
