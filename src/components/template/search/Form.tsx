1b1420256d6c1a1b56e02fd7803663b70d11eafa
sggonee <sggonee@gmail.com>
2025-11-11 19:12:42 +0900

    update form set search filter

--- src/components/template/search/Form.tsx
+++ src/components/template/search/Form.tsx
@@ -1,17 +1,21 @@
 import IconSearch from '@/assets/icons/icon-search.svg';
 import Button from '@/components/element/button';
+import Filter from '@/components/module/Filter';
+import { useState } from 'react';
 import style from './Form.module.css';
 
 const Search = () => {
+  const [openFilter, setOpenFilter] = useState(false);
   return (
     <div className={style.container}>
       <div className={style.box}>
         <input type="text" placeholder="검색어를 입력하세요" className={style.input} />
         <img src={IconSearch} alt="" />
       </div>
-      <Button variant="outline" size="sm">
+      <Button variant="outline" size="sm" onClick={() => setOpenFilter((prev) => !prev)}>
         상세검색
       </Button>
+      {openFilter && <Filter onClose={() => setOpenFilter(false)} />}
     </div>
   );
 };
