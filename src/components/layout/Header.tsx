badd31c589c4892ecc98e3358ea438fceb1bbc43
sggonee <sggonee@gmail.com>
2025-11-13 17:58:58 +0900

    update header default style

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,33 @@
-import { Link } from 'react-router-dom';
+import clsx from 'clsx';
+import { NavLink } from 'react-router-dom';
+import styles from './Header.module.css';
+
+const routes = [
+  {
+    label: '도서 검색',
+    to: '/search',
+  },
+  {
+    label: '내가 찜한 책',
+    to: '/like',
+  },
+];
 
 const Header = () => {
   return (
-    <header className="l-container">
-      <Link to="/search">도서 검색</Link>
-      <Link to="/like">내가 찜한 책</Link>
+    <header className={clsx(styles.container)}>
+      <h1 className={clsx(styles.logo, 'heading-lg')}>CERTICOS BOOKS</h1>
+      <nav className={styles.nav}>
+        {routes.map((route) => (
+          <NavLink
+            key={route.to}
+            to={route.to}
+            className={({ isActive }) => clsx('text-lg', isActive && styles['is--active'])}
+          >
+            {route.label}
+          </NavLink>
+        ))}
+      </nav>
     </header>
   );
 };
