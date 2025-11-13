9a9ffacf1eb7cee6026b9ebad2a35703da97b8a3
sggonee <sggonee@gmail.com>
2025-11-13 11:18:08 +0900

    add todo comment

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -5,6 +5,24 @@ import Summary from '@/components/template/search/Summary';
 import { useBooks } from '@/controller/books/query';
 import { useSearchParams } from 'react-router-dom';
 
+/**
+ * TODO
+ * [x] 목록 UI
+ * [x] 검색 UI
+ * [x] 상세 검색 팝업 UI
+ * [x] total count UI
+ * [x] 검색 히스토리
+ * [x] Not found
+ * [] 상세 검색 필터
+ * [] 무한 스크롤
+ * [] 찜하기
+ * [] heading 컴포넌트 분리
+ * [] 스타일 변수 정리
+ * [] 컴포넌트 이름 & 구조 정리
+ * [] 글로벌 네비게이션
+ * [] 아이콘 정리
+ */
+
 const Search = () => {
   const [searchParams] = useSearchParams();
   const query = searchParams.get('query') ?? '';
