6f6361b0ed03098ed148b01db043df9715d93e66
sggonee <sggonee@gmail.com>
2025-11-18 20:16:24 +0900

    enhance react compiler for auto memoization

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,13 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', {}]],
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
